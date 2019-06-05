257032db7a5843390181d481506184f16cc609ba
jasonpanelli <thejasonpanelli@gmail.com>
2019-06-04 17:57:40 -0700

    finished hopefully

--- reddit_model.py
+++ reddit_model.py
@@ -48,8 +48,9 @@ def main(context):
     submissions = context.read.parquet("submissions.parquet")
     labels= context.read.parquet("labels.parquet")
     '''
-    comments = comments.select("id", "body", "created_utc", "author_flair_text", "link_id")
-    submissions = submissions.select("title", "id")
+
+    comments = comments.select("id", "body", "created_utc", "author_flair_text", "link_id", col("score").alias("c_score"))
+    submissions = submissions.select("title", "id", col("score").alias("s_score"))
     labels = labels.select("Input_id", "labeldjt")
 
     # TASK 2: join labeled_data with comments_minimal
@@ -110,12 +111,12 @@ def main(context):
     posModel = posCrossval.fit(posTrain)
     print("Training negative classifier...")
     negModel = negCrossval.fit(negTrain)
-
+    
+    '''
     # Once we train the models, we don't want to do it again. We can save the models and load them again later.
     posModel.save("project2/pos.model")
     negModel.save("project2/neg.model")
-
-    '''
+    
     # To load saved models:
     posModel = CrossValidatorModel.load("project2/pos.model")
     negModel = CrossValidatorModel.load("project2/neg.model")
@@ -123,7 +124,7 @@ def main(context):
 
     # TASK 8: read more parts of comments
     fix_link_udf = udf(remove_first_three, StringType())
-    comments_fixed = comments.select(col("id").alias("comment_id"), fix_link_udf("link_id").alias("link_id"),"created_utc","body",col("author_flair_text").alias("state"))
+    comments_fixed = comments.select(col("id").alias("comment_id"), fix_link_udf("link_id").alias("link_id"),"created_utc","body",col("author_flair_text").alias("state"), "c_score")
     new_table, = submissions.join(comments_fixed, comments_fixed.link_id == submissions.id).randomSplit([0.2])
 
     # TASK 9:
@@ -131,35 +132,51 @@ def main(context):
     new_table = new_table.filter(~new_table.body.contains("&gt;") & ~new_table.body.contains("\s"))
     # Repeat task 4 and 5 and 6A
     sanitized_new_table = new_table.select("link_id", "state", "comment_id", "body", "created_utc", \
-            "title", split_udf(sanitize_udf("body")).alias("sanitized_text"))
+            "title", split_udf(sanitize_udf("body")).alias("sanitized_text"), "s_score", "c_score")
     final_table = cv_table.transform(sanitized_new_table)
 
     # Run the models
     ith = udf(ith_, FloatType())
-    task9_table = final_table.select("link_id", "state", "comment_id", "body", "created_utc", "title", "sanitized_text", col("vectors").alias("features"))
+    task9_table = final_table.select("link_id", "state", "comment_id", "body", "created_utc", "title", "c_score", "s_score", "sanitized_text", col("vectors").alias("features"))
     task9_table = posModel.transform(task9_table)
-    task9_table = task9_table.withColumn("pos", F.when(ith(task9_table.probability, lit(1)) > 0.2, 1).otherwise(0)).select("link_id", "state", "comment_id", "body", "created_utc", "title", "features", "pos")
+    task9_table = task9_table.withColumn("pos", F.when(ith(task9_table.probability, lit(1)) > 0.2, 1).otherwise(0)).select("link_id", "state", "comment_id", "body", "c_score", "s_score", "created_utc", "title", "features", "pos")
     task9_table = negModel.transform(task9_table)
-    task9_table = task9_table.withColumn("neg", F.when(ith(task9_table.probability, lit(1)) > 0.25, 1).otherwise(0)).select("link_id", "state", "comment_id", "body", "created_utc", "title", "pos", "neg")
+    task9_table = task9_table.withColumn("neg", F.when(ith(task9_table.probability, lit(1)) > 0.25, 1).otherwise(0)).select("link_id", "state", "comment_id", "body", "c_score", "s_score", "created_utc", "title", "pos", "neg")
 
     # TASK 10: calculate statistics
     # Part 1:
     part1 = task9_table.groupBy().agg(F.avg("pos").alias("pos"), F.avg("neg").alias("neg"))
     # Part 2:
     part2 = task9_table.groupBy(from_unixtime("created_utc","yyyy-MM-dd")).agg(F.avg("pos").alias("pos"), F.avg("neg").alias("neg"))
-    # Part 3:
+    # Part 3: 
     states = ['Alabama', 'Alaska', 'Arizona', 'Arkansas', 'California', 'Colorado', 'Connecticut', 'Delaware', 'District of Columbia', 'Florida', 'Georgia', 'Hawaii', 'Idaho', 'Illinois', 'Indiana', 'Iowa', 'Kansas', 'Kentucky', 'Louisiana', 'Maine', 'Maryland', 'Massachusetts', 'Michigan', 'Minnesota', 'Mississippi', 'Missouri', 'Montana', 'Nebraska', 'Nevada', 'New Hampshire', 'New Jersey', 'New Mexico', 'New York', 'North Carolina', 'North Dakota', 'Ohio', 'Oklahoma', 'Oregon', 'Pennsylvania', 'Rhode Island', 'South Carolina', 'South Dakota', 'Tennessee', 'Texas', 'Utah', 'Vermont', 'Virginia', 'Washington', 'West Virginia', 'Wisconsin', 'Wyoming']
-    part3 = task9_table.groupBy("state").agg(F.avg("pos").alias("pos"), F.avg("neg").alias("neg"))
+    part3 = task9_table.where(col("state").isin(states)).groupBy("state").agg(F.avg("pos").alias("pos"), F.avg("neg").alias("neg"))
     # Part 4:
-    part4 = task9_table.groupBy("link_id").agg(F.avg("pos").alias("pos"), F.avg("neg").alias("neg"))
-    # Part 5: calculate percentage of positive and negative comments by month
-    part5 = task9_table.groupBy(F.month(from_unixtime("created_utc","yyy-MM-dd"))).agg(F.avg("pos").alias("pos"), F.avg("neg").alias("neg"))
-    
-    part1.write.csv("part1.csv")
-    part2.write.csv("part2.csv")
-    part3.write.csv("part3.csv")
+    part4 = task9_table.groupBy("title").agg(F.avg("pos").alias("pos"), F.avg("neg").alias("neg"))
+    by_c_score = task9_table.groupBy("c_score").agg(F.avg("pos").alias("pos"), F.avg("neg").alias("neg"))
+    by_s_score = task9_table.groupBy("s_score").agg(F.avg("pos").alias("pos"), F.avg("neg").alias("neg"))
+   
+    # Save the data
+    part1.repartition(1).write.format("com.databricks.spark.csv").save("part1.csv")
+    part2.repartition(1).write.format("com.databricks.spark.csv").save("part2.csv")
+    part3.repartition(1).write.format("com.databricks.spark.csv").save("part3.csv")
+    by_c_score.repartition(1).write.format("com.databricks.spark.csv").save("c_score.csv")
+    by_s_score.repartition(1).write.format("com.databricks.spark.csv").save("s_score.csv")
     part4.repartition(1).write.format("com.databricks.spark.csv").save("part4.csv")
     part5.repartition(1).write.format("com.databricks.spark.csv").save("part5.csv")
+    
+    # Part 5: calculate percentage of positive and negative comments by month
+    time = context.read.csv("part2.csv")
+    time = time.withColumn("pos", time["_c1"].cast(FloatType()))\
+            .withColumn("neg", time["_c2"].cast(FloatType()))\
+            .drop("_c1").drop("_c2").na.drop()
+    part5 = time.groupBy(F.month(time._c0), F.year(time._c0)).agg(F.avg("pos").alias("pos"), F.avg("neg").alias("neg"))
+    
+    # for plot 4
+    part4.sort(col("pos").desc()).limit(10).repartition(1).write.format("com.databricks.spark.csv").save("plot4_pos.csv")
+    part4.sort(col("neg").desc()).limit(10).repartition(1).write.format("com.databricks.spark.csv").save("plot4_neg.csv")
+
+
 
 if __name__ == "__main__":
     conf = SparkConf().setAppName("CS143 Project 2B")
